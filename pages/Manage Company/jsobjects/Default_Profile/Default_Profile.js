ceb52d0404be383c1a8759b9adfe2edca0791bcd
aphiruth.s <aphiruth.s@baraadv.co.th>
2025-11-05 08:24:29 +0000

    Manage Company: new field of the company-profile, "INVOICE_GROUP_ID".

--- pages/Manage Company/jsobjects/Default_Profile/Default_Profile.js
+++ pages/Manage Company/jsobjects/Default_Profile/Default_Profile.js
@@ -15,4 +15,5 @@ export default {
 	FREQUENCY_TYPE:{data:"",regex:"",required:false,color:"" },
 	REPEAT_EVERY:{data:"",regex:"",required:false,color:"" },
 	FREQUENCY_DATE:{data:"",regex:"",required:false,color:"" },
+	INVOICE_GROUP_ID:{data:"",regex:"",required:false,color:"" },
 }
